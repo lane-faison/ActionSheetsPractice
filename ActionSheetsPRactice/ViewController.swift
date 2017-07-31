d5463e396674c3059c7e3b4b900b0a0c304175fb
Lane Faison <lane.faison@icloud.com>
2017-07-30 22:55:20 -0600

    Practice complete.

--- ActionSheetsPRactice/ViewController.swift
+++ ActionSheetsPRactice/ViewController.swift
@@ -10,13 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dayImageView: UIImageView!
+    @IBOutlet weak var nightImageView: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addGesture()
+    
+    }
+    
+    func addGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.showActionSheet))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func showActionSheet() {
+        let actionSheet = UIAlertController(title: "Select Time", message: "Day or Night?", preferredStyle: .actionSheet)
         
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
         
+        let day = UIAlertAction(title: "Day", style: .default) { action in
+            self.changeDayTime(isDay: true)
+        }
         
+        let night = UIAlertAction(title: "Night", style: .default) { action in
+            self.changeDayTime(isDay: false)
+        }
         
+        actionSheet.addAction(day)
+        actionSheet.addAction(night)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func changeDayTime(isDay: Bool) {
+        UIView.animate(withDuration: 0.4) {
+            self.dayImageView.alpha = isDay ? 1.0 : 0.0
+            self.nightImageView.alpha = isDay ? 0.0 : 1.0
+        }
     }
 
 
